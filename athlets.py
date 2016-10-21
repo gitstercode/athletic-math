24e6b3b4860a2b7b1df348cce97c966c1a5b3139
Varun Thalluru (V) <vthalluru@apple.com>
2016-10-21 16:57:37 -0700

    athlets.py in OOOP

--- athlets.py
+++ athlets.py
@@ -1,64 +1,72 @@
 import datetime
 from tabulate import tabulate
 
-athlets = input("Enter number of Athletes practicing: ")
+class AtheleteClass(object):
 
-players_dict = {}
+    def __init__(self):
+        self.athlets = input("Enter number of Athletes practicing: ")
 
-def convert_tup_to_seconds(list):
-    seconds_list = []
-    for i in list:
-        calSeconds = (i[0] * 60) + i[1]
-        seconds_list.append(calSeconds)
-    return seconds_list
+    def looping(self):
 
-def secs_to_MS(secs):
-    return datetime.datetime.fromtimestamp(secs).strftime('%M:%S')
+        players_dict = {}
 
-def calAvg(times):
-    seconds_list = convert_tup_to_seconds(times)
-    sum = 0
-    for i in seconds_list:
-        sum += i
-        sec = sum/float(len(seconds_list))
+        for i in range(1, self.athlets + 1):
 
-    return secs_to_MS(sec)
+            name = raw_input("Enter the name of Athlete " + str(i) + ": ")
 
-def calMax(times):
-    seconds_list = convert_tup_to_seconds(times)
-    sec = max(seconds_list)
-    return secs_to_MS(sec)
+            times = []
+            while True:
+                inp = input("Enter " + name + "\'s timing for 1000 meters or -1 to quit (minutes, seconds): ")
+                if inp == -1:
+                    break
+                times.append(inp)
 
-def calMin(times):
-    seconds_list = convert_tup_to_seconds(times)
-    sec = min(seconds_list)
-    return secs_to_MS(sec)
+            players_dict[name] = [self.calMin(times), self.calMax(times), self.calAvg(times)]
 
-for i in range(1, athlets+1):
+        whole_tablerdata = []
 
-    name = raw_input("Enter the name of Athlete " + str(i) + ": ")
+        for key, value in players_dict.iteritems():
+            tablerdata = list()
 
+            tablerdata.append(key)
+            tablerdata.append(value[0])
+            tablerdata.append(value[1])
+            tablerdata.append(value[2])
 
-    times = []
-    while True:
-        inp = input("Enter " + name + "\'s timing for 1000 meters or -1 to quit (minutes, seconds): ")
-        if inp == -1:
-            break
-        times.append(inp)
+            whole_tablerdata.append(tablerdata)
 
-    players_dict[name] = [calMin(times), calMax(times), calAvg(times)]
+        print "   "
+        print tabulate(whole_tablerdata, headers=["Athlete", "Min", "Max", "Avg"])
 
-whole_tablerdata = []
+    def convert_tup_to_seconds(self, lists):
+        seconds_list = []
+        for i in lists:
+            calSeconds = (i[0] * 60) + i[1]
+            seconds_list.append(calSeconds)
+        return seconds_list
 
-for key, value in players_dict.iteritems():
-    tablerdata = []
+    def secs_to_MS(self, secs):
+        return datetime.datetime.fromtimestamp(secs).strftime('%M:%S')
 
-    tablerdata.append(key)
-    tablerdata.append(value[0])
-    tablerdata.append(value[1])
-    tablerdata.append(value[2])
+    def calAvg(self, times):
+        seconds_list = self.convert_tup_to_seconds(times)
+        sum = 0
+        for i in seconds_list:
+            sum += i
+            sec = sum / float(len(seconds_list))
 
-    whole_tablerdata.append(tablerdata)
+        return self.secs_to_MS(sec)
 
-print "   "
-print tabulate(whole_tablerdata, headers=["Athlete","Min", "Max", "Avg"])
+    def calMax(self, times):
+        seconds_list = self.convert_tup_to_seconds(times)
+        sec = max(seconds_list)
+        return self.secs_to_MS(sec)
+
+    def calMin(self, times):
+        seconds_list = self.convert_tup_to_seconds(times)
+        sec = min(seconds_list)
+        return self.secs_to_MS(sec)
+
+obj = AtheleteClass()
+
+obj.looping()
